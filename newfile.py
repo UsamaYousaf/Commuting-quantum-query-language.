0dc05a89581424d8c4c690117efdf1eb9d617333
Usama Yousaf <80200194+UsamaYousaf@users.noreply.github.com>
2024-06-04 22:23:55 +0200

    new example

---
+++ newfile.py
@@ -0,0 +1,6 @@
+from sympy import symbols
+
+# Define your constants here
+VARIABLES = symbols('a b c d e f g')
+WEIGHTS = symbols('w_a w_b w_c w_d w_e w_f w_g')
+WEIGHTS = symbols('w_a w_b w_c w_d w_e w_f w_g w_l')
